cbe2f93bbf6c02b35794b1e0674f49f799f7b5c8
Sm1321 <siddulamohan1321@gmail.com>
2026-02-09 11:24:42 +0530

    Updated the SQL Window Function

--- Module 2 - SQL/3.WINDOW_FUNCTIONS/1.WINDOWS_FUNCTION.sql
+++ Module 2 - SQL/3.WINDOW_FUNCTIONS/1.WINDOWS_FUNCTION.sql
@@ -59,3 +59,76 @@ FROM Orders;
 SELECT OrderID,OrderDate,CustomerID,COUNT(*) OVER() TotalOrders,
 COUNT(*) OVER(PARTITION BY CustomerID) AS OrderByCustomers
 FROM Orders;
+
+-- Rank the orders based on their sales from highest to lowest
+SELECT OrderID,ProductID,Sales,
+ROW_NUMBER() OVER(ORDER BY sales DESC),
+RANK() OVER(ORDER BY sales DESC),
+DENSE_RANK() OVER(ORDER BY sales DESC)
+FROM Orders;
+
+-- Find the top higesht sales each product
+SELECT OrderID,ProductID,Sales,
+ROW_NUMBER() OVER(partition by ProductID ORDER BY sales DESC) as RankByProducts
+FROM Orders;
+
+-- Rank top one in each product
+SELECT * 
+FROM (
+SELECT OrderID,ProductID,Sales,
+ROW_NUMBER() OVER(partition by ProductID ORDER BY sales DESC) as RankByProducts
+FROM Orders 
+) as t WHERE t.RankByProducts = 1;
+
+
+-- Find the lowest 2 Customers based on the total sales 
+SELECT * 
+FROM (
+SELECT CustomerID,SUM(Sales) AS TotalSales,
+ROW_NUMBER() OVER(ORDER BY SUM(Sales)) as RankofCustomers
+FROM Orders 
+GROUP BY CustomerID
+) as t WHERE t.RankofCustomers <= 2;
+
+
+-- WINDOW LAG and LEAD Function
+-- Time series analysis
+-- Month over Month analysis
+-- Analyze the month-over-month perfomrance by finding the percentage change
+-- in sales between the current and previous months
+
+SELECT 
+    MONTH(OrderDate) AS OrderMonth,
+    SUM(Sales) AS CurrentMonthSales,
+    LAG(SUM(Sales)) OVER(ORDER BY MONTH(OrderDate)) AS PreviousMonthSales
+FROM Orders
+GROUP BY MONTH(OrderDate);
+
+
+SELECT *,
+       CurrentMonthSales - PreviousMonthSales AS MoM_change
+FROM (
+    SELECT 
+        OrderMonth,
+        CurrentMonthSales,
+        LAG(CurrentMonthSales) OVER (ORDER BY OrderMonth) AS PreviousMonthSales
+    FROM (
+        SELECT 
+            MONTH(OrderDate) AS OrderMonth,
+            SUM(Sales) AS CurrentMonthSales
+        FROM Orders
+        GROUP BY MONTH(OrderDate)
+    ) m
+) t;
+
+
+-- Find the lowest and highest sales for each product
+SELECT OrderID,ProductID,Sales,
+FIRST_VALUE(sales) OVER (PARTITION BY ProductID ORDER BY sales ) Lowestsales,
+LAST_VALUE(sales) OVER (PARTITION BY ProductID ORDER BY sales
+ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) HighestSales,
+FIRST_VALUE(Sales) OVER (PARTITION BY ProductID ORDER BY Sales DESC ) HighestSales2,
+MIN(sales) OVER (PARTITION BY ProductID) LowestSales2,
+MAX(sales) OVER (PARTITION BY ProductID) LowestSales3
+FROM Orders;
+
